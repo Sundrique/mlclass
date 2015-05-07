e33b03f21b8f8c362ecf62d735b3dfe1344b0597
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-05-07 21:01:35 +0700

    Write plot data  and sigmoid

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -4,25 +4,12 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
-figure; hold on;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
-
+figure;
+hold on;
+pos = find(y==1);
+neg = find(y==0);
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2,'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 hold off;
 
--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -3,16 +3,6 @@ function g = sigmoid(z)
 %   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
-
-
-
-
-
-% =============================================================
+g = (exp(-z) + 1).^-1;
 
 end
