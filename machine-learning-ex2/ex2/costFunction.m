56fb97d8983458039bd44cb7f8b60ac76bfb3e5c
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-05-07 22:02:22 +0700

    Implement cost function and gradient

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -6,27 +6,9 @@ function [J, grad] = costFunction(theta, X, y)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+h = sigmoid(theta' * X');
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
+J = -(log(h) * y + log(1 - h) * (1 - y)) / m;
+grad = ((h - y') * X ./ m)';
 
 end
