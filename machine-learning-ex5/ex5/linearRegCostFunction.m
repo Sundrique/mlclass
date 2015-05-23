86af11c5ce8086e14ac4b135876f83c650a04c05
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-05-23 22:15:15 +0700

    Implement linear regression cost function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -8,29 +8,13 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+thetaReg = [0; theta(2:end)];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+h = X * theta;
 
-
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
+% You need to return the following variables correctly
+J = (sum((h - y) .^ 2) + sum(thetaReg .^ 2) * lambda) / 2 / m;
+grad = (X' * (h - y) + lambda * thetaReg) / m;
 
 grad = grad(:);
 
