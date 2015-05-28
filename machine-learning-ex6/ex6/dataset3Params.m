b1cead1fedc333a1ade751990bf19b042c08e43c
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-05-28 22:35:32 +0700

    Select best C and sigma from this list using cross validation error

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -7,28 +7,20 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+Cs = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmas = [0.01 0.03 0.1 0.3 1 3 10 30];
+errors = zeros(length(Cs), length(sigmas));
+for i = 1:length(Cs);
+    for j = 1:length(sigmas)
+        model= svmTrain(X, y, Cs(i), @(x1, x2) gaussianKernel(x1, x2, sigmas(j)));
+        predictions = svmPredict(model, Xval);
+        errors(i,j) = mean(double(predictions ~= yval));
+    end;
+end;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-
-
-
-
-
-
-% =========================================================================
+[M, I] = min(errors(:));
+[I_row, I_col] = ind2sub(size(errors),I);
+C = Cs(I_row);
+sigma = sigmas(I_col);
 
 end
