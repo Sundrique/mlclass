7e5b42194da4f184b4288e4e1f45b504c0689a6b
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-11-15 15:48:03 +0700

    Fix exercise #2 cost function

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -5,14 +5,14 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   gradient of the cost w.r.t. to the parameters. 
 
 m = length(y); % number of training examples
+n = length(theta);
 h = sigmoid(theta' * X');
 
-thetaReg = [0 theta(2:m)];
-thetaReg(1) = 0;
+thetaReg = [0; theta(2:n)];
 
 % You need to return the following variables correctly
 J = -(log(h) * y + log(1 - h) * (1 - y) - lambda / 2 * sum(thetaReg .^ 2)) / m;
 
-grad = ((h - y') * X + lambda * thetaReg)' / m;
+grad = ((h - y') * X + lambda * thetaReg')' / m;
 
 end
